eec5500b68ff08ce154da551dcf16819a12c8d20
felixdusengimana <phelixdusengimana@gmail.com>
2024-07-11 19:19:26 +0200

    feat: change Ip address

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,10 @@
-# setup client SSH configuration
+# Client configuration file with Puppet
+file_line { 'Turn_off_passwd_auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+}
 
-exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
-        path    => '/bin/'
+file_line { 'Declare_identity_file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
 }

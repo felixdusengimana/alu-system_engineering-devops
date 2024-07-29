81ca5038946998c5e678b60cfeef32a5eaa0c5ed
felixdusengimana <phelixdusengimana@gmail.com>
2024-07-29 14:52:58 +0200

    fix limiter

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,14 @@
-exec { 'change-os-configuration-for-holberton-user':
+# This Puppet manifest adjusts the OS configuration to increase the file descriptor limits for the holberton user.
+
+exec { 'increase-file-descriptor-limits':
   command => 'echo "holberton soft nofile 4096\nholberton hard nofile 8192" >> /etc/security/limits.conf &&
               echo "session required pam_limits.so" >> /etc/pam.d/common-session &&
               echo "session required pam_limits.so" >> /etc/pam.d/common-session-noninteractive &&
               echo "ulimit -n 4096" >> /etc/profile',
   path    => ['/bin', '/usr/bin'],
+  unless  => 'grep -q "holberton soft nofile 4096" /etc/security/limits.conf &&
+              grep -q "holberton hard nofile 8192" /etc/security/limits.conf &&
+              grep -q "session required pam_limits.so" /etc/pam.d/common-session &&
+              grep -q "session required pam_limits.so" /etc/pam.d/common-session-noninteractive &&
+              grep -q "ulimit -n 4096" /etc/profile',
 }
